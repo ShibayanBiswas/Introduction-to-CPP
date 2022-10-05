bffb778727a4855cbf3af27711f550321a332b93
ShibayanBiswas <114879361+ShibayanBiswas@users.noreply.github.com>
2022-10-06 01:17:59 +0530

    Add files via upload

---
+++ Getting Started/Variables and Datatypes/Add_two_numbers.cpp
@@ -0,0 +1,10 @@
+#include <iostream>  // For executing inbuilt commands
+using namespace std;  // For declaring a namespace
+
+int main(){
+    int a=10;
+    int b=15;
+    int c=a+b;
+
+    cout << c << endl;
+}
