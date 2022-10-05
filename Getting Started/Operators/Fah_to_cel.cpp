014cfea736a7272fec2cb270d70bde5844a29efa
ShibayanBiswas <114879361+ShibayanBiswas@users.noreply.github.com>
2022-10-06 01:14:46 +0530

    Add files via upload

---
+++ Getting Started/Operators/Fah_to_cel.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double f;
+    cout << "Enter the Fahrenheit value" << endl;
+    cin >> f;
+    double c=(5.0/9)*(f-32);
+    cout << "The Celcius value is " << c << endl;
+}
