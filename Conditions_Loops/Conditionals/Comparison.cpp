0c568db532aa0f67e1f8f7f7289c4ec9a9b5aa13
ShibayanBiswas <114879361+ShibayanBiswas@users.noreply.github.com>
2022-10-06 00:56:28 +0530

    Add files via upload

---
+++ Conditions_Loops/Conditionals/Comparison.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    cout << "Enter the two numbers" << endl;
+    cin >> a >> b;
+    if (a==b){
+        cout << "Both of them are equal" << endl;
+    }
+    else{
+        cout << "They are not equal" << endl;
+    }
+}
